46458bbb1e918c4c494954f5c0654c06c1a0ac38
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-27 17:27:24 -0400

    Translate tetra

--- source/shape/tetra.d
+++ source/shape/tetra.d
@@ -1,56 +1,59 @@
 module shape.tetra;
 
-import { SHAPE_TETRA, AABB_PROX } from '../constants';
-import { Shape } from './Shape';
+import constants;
+import shape.shape;
+import shape.shape_config;
+import math.vec3;
 
 /**
  * A tetra shape.
  * @author xprogram
  */
-function Tetra ( config, p1, p2, p3, p4 ){
 
-    Shape.call( this, config );
-    this.type = SHAPE_TETRA;
+public class Tetra : Shape {
 
-    // Vertices and faces of tetra
-    this.verts = [ p1, p2, p3, p4 ];
-    this.faces = [ this.mtri(0, 1, 2), this.mtri(1, 2, 3), this.mtri(2, 3, 4), this.mtri(4, 0, 1) ];
+    float[] verts;
+    /// This was originally an anonymous function with a,b,c instead of x,y,z
+    Vec3[] faces;
 
-};
+    this( ShapeConfig config, float p1, float p2, float p3, float p4 ){
 
-Tetra.prototype = Object.create( Shape.prototype );
-Tetra.prototype.constructor = TetraShape;
+        super(config);
 
-Tetra.prototype.calculateMassInfo = function( out ){
-    // I guess you could calculate box mass and split it
-    // in half for the tetra...
-    this.aabb.setFromPoints(this.verts);
-    var p = this.aabb.elements;
-    var x = p[3] - p[0];
-    var y = p[4] - p[1];
-    var z = p[5] - p[2];
-    var mass = x * y * z * this.density;
-    var divid = 1/12;
-    out.mass = mass;
-    out.inertia.set(
-        mass * ( 2*y*2*y + 2*z*2*z ) * divid, 0, 0,
-        0, mass * ( 2*x*2*x + 2*z*2*z ) * divid, 0,
-        0, 0, mass * ( 2*y*2*y + 2*x*2*x ) * divid
-    );
+        this.type = SHAPE_TETRA;
 
-};
+        // Vertices and faces of tetra
+        this.verts = [ p1, p2, p3, p4 ];
+        this.faces = [ new Vec3(0, 1, 2), new Vec3(1, 2, 3), new Vec3(2, 3, 4), new Vec3(4, 0, 1) ];
 
-Tetra.prototype.updateProxy = function () {
+    }
 
-    this.aabb.setFromPoints(this.verts);
-    this.aabb.expandByScalar(AABB_PROX);
+    void calculateMassInfo ( Shape output ){
+        // I guess you could calculate box mass and split it
+        // in half for the tetra...
+        this.aabb.setFromPoints(this.verts);
+        var p = this.aabb.elements;
+        var x = p[3] - p[0];
+        var y = p[4] - p[1];
+        var z = p[5] - p[2];
+        var mass = x * y * z * this.density;
+        var divid = 1/12;
+        output.mass = mass;
+        output.inertia.set(
+            mass * ( 2*y*2*y + 2*z*2*z ) * divid, 0, 0,
+            0, mass * ( 2*x*2*x + 2*z*2*z ) * divid, 0,
+            0, 0, mass * ( 2*y*2*y + 2*x*2*x ) * divid
+        );
 
-    if(this.proxy !== null) this.proxy.update();
+    }
 
-};
+    void updateProxy() {
 
-Tetra.prototype.mtri = function ( a, b, c ){
-    return {a: a, b: b, c: c};
+        this.aabb.setFromPoints(this.verts);
+        this.aabb.expandByScalar(AABB_PROX);
+
+        if(this.proxy != null) this.proxy.update();
+
+    }
+
 }
-
-export { Tetra };
