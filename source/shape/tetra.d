0d1967b8c2cfc420b5db0d3c426470ccf0d0bf82
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-27 17:45:15 -0400

    Fix tetra

--- source/shape/tetra.d
+++ source/shape/tetra.d
@@ -12,11 +12,11 @@ import math.vec3;
 
 public class Tetra : Shape {
 
-    float[] verts;
+    Vec3[] verts;
     /// This was originally an anonymous function with a,b,c instead of x,y,z
     Vec3[] faces;
 
-    this( ShapeConfig config, float p1, float p2, float p3, float p4 ){
+    this( ShapeConfig config, Vec3 p1, Vec3 p2, Vec3 p3, Vec3 p4 ){
 
         super(config);
 
@@ -33,12 +33,12 @@ public class Tetra : Shape {
         // I guess you could calculate box mass and split it
         // in half for the tetra...
         this.aabb.setFromPoints(this.verts);
-        var p = this.aabb.elements;
-        var x = p[3] - p[0];
-        var y = p[4] - p[1];
-        var z = p[5] - p[2];
-        var mass = x * y * z * this.density;
-        var divid = 1/12;
+        float[] p = this.aabb.elements;
+        float x = p[3] - p[0];
+        float y = p[4] - p[1];
+        float z = p[5] - p[2];
+        float mass = x * y * z * this.density;
+        float divid = 1/12;
         output.mass = mass;
         output.inertia.set(
             mass * ( 2*y*2*y + 2*z*2*z ) * divid, 0, 0,
@@ -54,7 +54,7 @@ public class Tetra : Shape {
         this.aabb.setFromPoints(this.verts);
         this.aabb.expandByScalar(AABB_PROX);
 
-        if(this.proxy != null) this.proxy.update();
+        if(this.proxy !is null) this.proxy.update();
 
     }
 
